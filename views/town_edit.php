177c4556fe83d059d4cf54fcc1bbc1e09a2a414b
Dean Sabroso <93834480+Deanx44@users.noreply.github.com>
2023-12-06 07:31:05 +0800

    Update town_edit.php

--- views/town_edit.php
+++ views/town_edit.php
@@ -1,63 +1,61 @@
 <?php
-include_once("../db.php"); // Include the Database class file
-include_once("../town_city.php"); // Include the Student class file
+include_once("../db.php"); 
+include_once("../town_city.php"); 
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Fetch town data by ID from the database
+    
     $db = new Database();
     $town_city = new TownCity($db);
-    $town_data = $town_city->read($id); // Implement the read method in the Town class
+    $town_city_Data = $town_city->read($id); 
 
-    if (!$town_data) {
-        echo "Town not found.";
-    }
-} else {
-    echo "Town ID not provided.";
-}
+   
+} 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = [
-        'id' => $_POST['id'],
+        'id' => $_POST['id'],  
         'name' => $_POST['name'],
     ];
 
     $db = new Database();
     $town_city = new TownCity($db);
 
-    // Call the edit method to update the student data
+    
     if ($town_city->update($id, $data)) {
         echo "Record updated successfully.";
-        header('Location: towns.view.php');
     } else {
         echo "Failed to update the record.";
     }
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
-    <title>Edit Town City</title>
+    <title>Edit Town</title>
 </head>
 <body>
-<!-- Include the header and navbar -->
-<?php include('../includes/navbar.php'); ?>
+    
+    <?php include('../templates/header.html'); ?>
+    <?php include('../includes/navbar.php'); ?>
 
-<div class="content">
-    <h2>Edit Town Name</h2>
-    <form action="#" method="post" name="form_edit">
-        <input type="hidden" name="id" value="<?php echo $town_data['id']; ?>"/>
-
-        <label for="name">Town Name:</label>
-        <input type="text" name="name" id="name" value="<?php echo $town_data['name']; ?>"/>
-
+    <div class="content">
+    <h2>Edit Town Information</h2>
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?php echo $town_city_Data['id']; ?>">
+        
+        
+        <label for="town_city">Name: </label>
+        <input type="text" name="name" id="name" value="<?php echo $town_city_Data['name']; ?>">
+        
         <input type="submit" value="Update">
     </form>
-</div>
-<?php include('../templates/footer.html'); ?>
+    </div>
+    <?php include('../templates/footer.html'); ?>
 </body>
 </html>
+
