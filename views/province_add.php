0005f1304504d9036fcbdfdcfc89c2d95c5527d0
Dean Sabroso <93834480+Deanx44@users.noreply.github.com>
2023-12-05 12:27:21 +0800

    Update province_add.php

--- views/province_add.php
+++ views/province_add.php
@@ -1,23 +1,21 @@
 <?php
 include_once("../db.php"); 
-include_once("../student_details.php"); // 
-include_once("../province.php"); 
+include_once("../province.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $data = [
-        'name' => $_POST['name'],
+    $data = [    
+    
+    'name' => $_POST['name'],
     ];
 
     
     $database = new Database();
-    $province = new Province($database);
-    $province_id = $province->create($data);
-
-    header("Location: province.view.php");
+    $town_city = new Province($database);
+    $town_city_id = $town_city->create($data);
+    
+    echo "Record added successfully.";
 }
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,23 +23,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
 
-    <title>Add Province Data</title>
+    <title>Add Student Data</title>
 </head>
 <body>
-<!-- Include the header and navbar -->
-<?php include('../includes/navbar.php'); ?>
+    
+    <?php include('../templates/header.html'); ?>
+    <?php include('../includes/navbar.php'); ?>
 
-<div class="content">
-    <h1>Add Province Data</h1>
-    <form action="#" method="post" class="centered-form">
+    <div class="content">
+    <h1>Add Province</h1>
+    <form action="" method="post" class="centered-form">
 
-        <label for="name">Province Name</label>
-        <input type="text" id="name" name="name" required>
+
+        <label for="name">Province Name:</label>
+        <input type="text" name="name" id="name" required>
 
         <input type="submit" value="Add Province">
     </form>
-</div>
-
-<?php include('../templates/footer.html'); ?>
+    </div>
+    
+    <?php include('../templates/footer.html'); ?>
 </body>
 </html>
