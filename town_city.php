a97de4e5387f7078b68ba5e50101f73dad99a77e
Dean Sabroso <93834480+Deanx44@users.noreply.github.com>
2023-12-05 12:00:20 +0800

    Update town_city.php

--- town_city.php
+++ town_city.php
@@ -1,5 +1,6 @@
 <?php
-include("db.php"); 
+include_once("db.php");
+
 class TownCity {
     private $db;
 
@@ -8,76 +9,35 @@ class TownCity {
     }
 
     public function getAll() {
-        $recordsPerPage = 10;
-        $page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
-        $search = isset($_GET['search']) ? $_GET['search'] : '';
-        $offset = ($page - 1) * $recordsPerPage;
-
         try {
-            $sql = "SELECT * FROM town_city tc";
-
-            if (!empty($search)) {
-                $searchParam = "%$search%";
-                $sql .= "
-                            WHERE tc.name LIKE :searchParam
-                        ";
-            }
-
-            $sql .= " ORDER BY tc.name LIMIT :limit OFFSET :offset";
-
+            $sql = "SELECT * FROM town_city";
             $stmt = $this->db->getConnection()->prepare($sql);
-
-            if (!empty($search)) {
-                $stmt->bindParam(':searchParam', $searchParam, PDO::PARAM_STR);
-            }
-
-            $stmt->bindValue(':limit', $recordsPerPage, PDO::PARAM_INT);
-            $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
-
             $stmt->execute();
 
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
-            
-            throw $e; 
+
+            throw $e;
         }
     }
-	
-	public function displayAll() {
-		try {
-            $sql = "SELECT * FROM town_city";
-
-            $stmt = $this->db->getConnection()->prepare($sql);
-            $stmt->execute();
-
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            
-            throw $e; 
-        }
-	}
-
     public function create($data) {
         try {
             
             $sql = "INSERT INTO town_city(name) VALUES(:name);";
             $stmt = $this->db->getConnection()->prepare($sql);
-
-            
+            // Bind values to placeholders
             $stmt->bindParam(':name', $data['name']);
-
             
             $stmt->execute();
-
-            // Check if the insert was successful
-
+            
+             
             if($stmt->rowCount() > 0)
             {
                 return $this->db->getConnection()->lastInsertId();
             }
 
         } catch (PDOException $e) {
-            
+            // Handle any potential errors here
             echo "Error: " . $e->getMessage();
             throw $e; 
         }
@@ -93,9 +53,9 @@ class TownCity {
             $stmt->execute();
 
             
-            $town_city = $stmt->fetch(PDO::FETCH_ASSOC);
+            $studentData = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            return $town_city;
+            return $studentData;
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
             throw $e; 
@@ -105,13 +65,14 @@ class TownCity {
     public function update($id, $data) {
         try {
             $sql = "UPDATE town_city SET
-                    name = :name
+                    name = :name 
                     WHERE id = :id";
 
             $stmt = $this->db->getConnection()->prepare($sql);
             
             $stmt->bindValue(':id', $data['id']);
             $stmt->bindValue(':name', $data['name']);
+        
 
             
             $stmt->execute();
@@ -122,7 +83,6 @@ class TownCity {
             throw $e; 
         }
     }
-
     public function delete($id) {
         try {
             $sql = "DELETE FROM town_city WHERE id = :id";
@@ -138,40 +98,8 @@ class TownCity {
             }
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
-            throw $e; 
-        }
-    }
-
-    public function getTownCount() {
-        $conn = $this->db->getConnection();
-
-        $search = isset($_GET['search']) ? $_GET['search'] : '';
-
-        try {
-            $sql = "SELECT COUNT(*) as total FROM town_city tc";
-
-            if (!empty($search)) {
-                $searchParam = "%$search%";
-                $sql .= "
-                            WHERE tc.name LIKE :searchParam
-                        ";
-            }
-
-            $stmt = $conn->prepare($sql);
-
-            if (!empty($search)) {
-                $stmt->bindParam(':searchParam', $searchParam, PDO::PARAM_STR);
-            }
-
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $result[0]['total'];
-        } catch (PDOException $e) {
-            
-            echo "Error: " . $e->getMessage();
-            throw $e; 
+            throw $e;
         }
     }
 }
-
 ?>
