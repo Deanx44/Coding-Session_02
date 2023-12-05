9cf12024ae9fefe42c5c0fe550e923409a28917a
Dean Sabroso <93834480+Deanx44@users.noreply.github.com>
2023-12-05 12:30:06 +0800

    Update students.view.php

--- views/students.view.php
+++ views/students.view.php
@@ -1,13 +1,11 @@
 <?php
-include("../db.php");
-include("../student.php");
-include("../student_details.php");
+include_once("../db.php");
+include_once("../student.php");
 
 $db = new Database();
 $connection = $db->getConnection();
 $student = new Student($db);
 
-$search = isset($_GET['search']) ? $_GET['search'] : '';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,117 +14,63 @@ $search = isset($_GET['search']) ? $_GET['search'] : '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Records</title>
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <!-- Include the header -->
+
+    <?php include('../templates/header.html'); ?>
     <?php include('../includes/navbar.php'); ?>
-	
+
     <div class="content">
-		<div class="flex">
-          <h2 style="width:25%">Student Records</h2>
-		  <form method="GET" action="#" id="searchForm" class="searchForm">
-		    <div class="container-search">
-		      <input type="text" placeholder="Search" name="search" class="search" id="searchInput">
-		      <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
-			    <path d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z" fill-rule="evenodd"></path>
-		      </svg>
-		    </div>
-		  </form>
-		</div>
-        <table class="orange-theme">
-            <thead>
-                <tr>
-                    <th>Student Number</th>
-                    <th>Full Name</th>
-                    <th>Contact Number</th>
-                    <th>Gender</th>
-                    <th>Birthdate</th>
-                    <th>Street</th>
-                    <th>Town ID</th>
-                    <th>Province ID</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
+    <h2>Student Records</h2>
+    <table class="orange-theme">
+        <thead>
+            <tr>
+                <th>Student Number</th>
+                <th>First Name</th>
+                <th>Middle Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Birthdate</th>
+                <th>Contact</th>
+                <th>Street</th>
+                <th>Town_City</th>
+                <th>Province</th>
+                <th>Zip Code</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
 
-                <?php
-                $results = $student->displayAllWithDetails();
-                foreach ($results as $result) {
-                ?>
-                <tr>
-                    <td><?php echo $result['student_number']; ?></td>
-                    <td><?php echo $result['first_name'] . " " . $result['middle_name'] . " " . $result['last_name']; ?></td>
-                    <td><?php echo $result['contact_number']; ?></td>
-                    <td><?php if($result['gender'] == '0') {echo 'M';} else {echo 'F';}?></td>
-                    <td><?php echo date("M j Y", strtotime($result['birthday'])); ?></td>
-                    <td><?php echo $result['street']; ?></td>
-                    <td><?php echo $result['town_city']; ?></td>
-                    <td><?php echo $result['province']; ?></td>
-                    <td>
-                        <a href="student_edit.php?id=<?php echo $result['id']; ?>"><i style="font-size:24px;color:forestgreen" class="fa fa-edit"></i></a>
-                        <a href="student_delete.php?id=<?php echo $result['id']; ?>"><i style="font-size:24px;color:orangered" class="fa fa-trash"></i></a>
-                    </td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-		<div class="flex-between">
-          <a class="button-link" href="student_add.php">Add New Record</a>
-		  <button class="button-link" id="refreshButton">Refresh</button>
-		</div>
-		
-        <!--Pagination Section-->
-        <div class="pagination">
             <?php
-                $row = $student->getStudentsCount();
-                $totalPages = ceil($row / 1000);
-
-                for ($i = 1; $i <= $totalPages; $i++) {
-                    echo '<a href="?page=' . $i . '&search=' . urlencode($search) . '" class="page-link">' . $i . '</a>';
-                }
+            $results = $student->displayAll(); 
+            foreach ($results as $result) {
             ?>
-        </div>
+            <tr>
+                <td><?php echo $result['student_number']; ?></td>
+                <td><?php echo $result['first_name']; ?></td>
+                <td><?php echo $result['middle_name']; ?></td>
+                <td><?php echo $result['last_name']; ?></td>
+                <td><?php echo ($result['gender'] == '0') ? 'F' : 'M'; ?></td>
+                <td><?php echo date('M j Y', strtotime($result['birthday'])); ?></td>
+                <td><?php echo $result['contact_number']; ?></td>
+                <td><?php echo $result['street']; ?></td>
+                <td><?php echo $result['town_city']; ?></td>
+                <td><?php echo $result['province']; ?></td>
+                <td><?php echo $result['zip_code']; ?></td>
+                <td>
+                    <a href="student_edit.php?id=<?php echo $result['id']; ?>">Edit</a>
+                    |
+                    <a href="student_delete.php?id=<?php echo $result['id']; ?>">Delete</a>
+                </td>
+            </tr>
+            
+        <?php } ?>
+        
+   </tbody>
+    </table>
+        
+    <a class="button-link" href="student_add.php">Add New Record</a>
     </div>
     <?php include('../templates/footer.html'); ?>
 </body>
 </html>
-
-<!--Scripts-->
-<script>
-    const searchInput = document.getElementById('searchInput');
-
-	searchInput.addEventListener('keydown', function (event) {
-		if (event.key === 'Enter') {
-			updateURL();
-			event.preventDefault(); 
-		}
-	});
-	
-	refreshButton.addEventListener('click', function () {
-		clearSearch();
-	});
-	
-	window.addEventListener('popstate', function (event) {
-		location.reload();
-	});
-
-	function updateURL() {
-		const searchTerm = searchInput.value.trim();
-		const newURL = window.location.origin + window.location.pathname + '?search=' + encodeURIComponent(searchTerm);
-		
-		window.history.pushState({ path: newURL }, '', newURL);
-
-		const form = document.getElementById('searchForm');
-		form.submit();
-	}
-	
-	function clearSearch() {
-		searchInput.value = '';
-
-		const newURL = window.location.origin + window.location.pathname;
-		window.history.pushState({ path: newURL }, '', newURL);
-
-		location.reload();
-	}
-</script>
