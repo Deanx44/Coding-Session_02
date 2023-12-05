27cfca17c89f76f386144c0ba92ff09e87b23591
Dean Sabroso <93834480+Deanx44@users.noreply.github.com>
2023-12-06 07:30:23 +0800

    Update town_delete.php

--- views/town_delete.php
+++ views/town_delete.php
@@ -1,20 +1,17 @@
 <?php
-include("../db.php"); 
-include("../town_city.php"); 
+include_once("../db.php"); 
+include_once("../town_city.php"); 
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
     $id = $_GET['id']; 
-
     
     $db = new Database();
     $town_city = new TownCity($db);
 
-    
     if ($town_city->delete($id)) {
         echo "Record deleted successfully.";
     } else {
         echo "Failed to delete the record.";
     }
-    header("Location: towns.view.php");
 }
 ?>
